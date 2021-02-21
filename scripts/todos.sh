54bb57591a7b35fa95816f6cd65bedc1d17c957e
David Härer <haerer@locandis.de>
2021-02-21 22:51:32 +0100

    Generate sections for each topic.

--- scripts/todos.sh
+++ scripts/todos.sh
@@ -3,9 +3,24 @@
 echo "# TODO" > TODO.md
 echo "" >> TODO.md
 
-git grep "TODO: " source \
+git grep "TODO: " source/index.rst \
     | sed 's/\([^:]*\):TODO: \(.*\)/- [ ] [\2](\1)/' \
     >> TODO.md
 
 echo "" >> TODO.md
+echo "## Computers" >> TODO.md
+echo "" >> TODO.md
 
+git grep "TODO: " source/computers \
+    | sed 's/\([^:]*\):TODO: \(.*\)/- [ ] [\2](\1)/' \
+    >> TODO.md
+
+echo "" >> TODO.md
+echo "## Music" >> TODO.md
+echo "" >> TODO.md
+
+git grep "TODO: " source/music \
+    | sed 's/\([^:]*\):TODO: \(.*\)/- [ ] [\2](\1)/' \
+    >> TODO.md
+
+echo "" >> TODO.md
