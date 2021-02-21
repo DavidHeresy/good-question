1bbfb73b1af50dc59d9c7301f25a860e85b34a12
David Härer <haerer@locandis.de>
2021-02-21 12:04:52 +0100

    Add sphinx.ext.githubpages.

--- source/conf.py
+++ source/conf.py
@@ -28,6 +28,7 @@ author = 'David Härer'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -44,8 +45,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_baseurl = "good-question"
 html_theme = "sphinx_book_theme"
+html_baseurl = 'good-question.davidheresy.de'
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
